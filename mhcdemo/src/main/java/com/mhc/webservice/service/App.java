7f4c75279dcf1d2f25430c75904e0e06b23d20ff
DNCBA <354710644@qq.com>
2018-11-01 23:38:42 +0800

    webService相关

--- mhcdemo/src/main/java/com/mhc/webservice/service/App.java
+++ mhcdemo/src/main/java/com/mhc/webservice/service/App.java
@@ -8,7 +8,8 @@ public class App {
 
     public static void main(String[] args) {
 
-        Endpoint.publish("localhost:8080/userService",new UserServiceImpl());
+        Endpoint.publish("http://localhost:8080/userService",new UserServiceImpl());
 
+        System.out.println("webservice success ...");
     }
 }
