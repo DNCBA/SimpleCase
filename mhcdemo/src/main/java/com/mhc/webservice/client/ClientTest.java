447908ee76e412e027cf6dbb28a4b94e72d79218
caomenghui <coamenghui1@huawei.com>
2018-11-13 18:20:05 +0800

    webservice相关代码

---
+++ mhcdemo/src/main/java/com/mhc/webservice/client/ClientTest.java
@@ -0,0 +1,11 @@
+package com.mhc.webservice.client;
+
+public class ClientTest {
+
+    public static void main(String[] args) {
+        UserServiceImplService service = new UserServiceImplService();
+        UserServiceImpl userServiceImplPort = service.getUserServiceImplPort();
+        User user = userServiceImplPort.find();
+        System.out.println(user);
+    }
+}
---
+++ mhcdemo/src/main/java/com/mhc/webservice/client/FindResponse.java
@@ -0,0 +1,62 @@
+
+package com.mhc.webservice.client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>findResponse complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType name="findResponse">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="return" type="{http://impl.service.webservice.mhc.com/}user" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "findResponse", propOrder = {
+    "_return"
+})
+public class FindResponse {
+
+    @XmlElement(name = "return")
+    protected User _return;
+
+    /**
+     * 获取return属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link User }
+     *     
+     */
+    public User getReturn() {
+        return _return;
+    }
+
+    /**
+     * 设置return属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link User }
+     *     
+     */
+    public void setReturn(User value) {
+        this._return = value;
+    }
+
+}
