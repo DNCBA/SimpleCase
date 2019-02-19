6eb908096a4c4c33af2260bc2a140c59e865435c
mhc <354710644@qq.com>
2019-02-19 17:10:49 +0800

    java8

---
+++ mhcdemo/src/main/java/com/mhc/java8/StreamDemo.java
@@ -0,0 +1,42 @@
+package com.mhc.java8;
+
+import org.joda.time.Interval;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamDemo {
+
+    public static void main(String[] args) {
+        Stream<Integer> stream = createStream();
+        Map<String, Long> collect = stream.filter(t -> t > 5)
+                .collect(Collectors.groupingBy(Object::toString, Collectors.counting()));
+
+
+        System.out.println(collect);
+
+    }
+
+
+    public static Stream createStream() {
+
+
+        Stream streamInstance = null;
+
+        List<Integer> list = Arrays.asList(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 5, 8});
+        streamInstance = list.stream();
+
+//        streamInstance = Stream.of(1,2,3,4,5,6,7,8,9,10,1);
+//
+//        streamInstance = Arrays.stream(new Integer[]{1,2,3,4,5,6,7,8,9,1,2,5,8});
+//
+//        streamInstance = Stream.iterate(0, n -> n +2) ;
+//
+//        streamInstance = Stream.generate(Math::random);
+
+        return streamInstance;
+    }
+
+}
