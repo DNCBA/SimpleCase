09838bd60700840fc07eb561d72947265133f744
mhc <354710644@qq.com>
2019-02-20 09:30:54 +0800

    java8

--- mhcdemo/src/main/java/com/mhc/java8/StreamDemo.java
+++ mhcdemo/src/main/java/com/mhc/java8/StreamDemo.java
@@ -9,13 +9,28 @@ import java.util.stream.Stream;
 
 public class StreamDemo {
 
+    /**
+     * reducing(初始值，转换函数，累积函数)
+     * @param args
+     */
     public static void main(String[] args) {
         Stream<Integer> stream = createStream();
-        Map<String, Long> collect = stream.filter(t -> t > 5)
-                .collect(Collectors.groupingBy(Object::toString, Collectors.counting()));
+//        Map<String, Long> collect = stream.filter(t -> t > 5)
+//                .collect(Collectors.groupingBy(Object::toString, Collectors.counting()));
+//        System.out.println(collect);
+
+//        Integer sum = stream.filter(t -> t > 5)
+//                .map(Integer::valueOf)
+//                .collect(Collectors.summingInt(Integer::new));
 
 
-        System.out.println(collect);
+        Integer sum = stream.filter(t -> t > 5)
+                .map(Objects::toString)
+                .collect(Collectors.reducing(0,
+                        Integer::parseInt,
+                        (a, b) -> a + b));
+
+        System.out.println(sum);
 
     }
 
