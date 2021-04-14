59f1f9eb9aaafad358be5e03bcbe3f20ae05b5a6
menghui.cao <menghui.cao@leyantech.com>
2021-04-14 15:47:44 +0800

    feat: add

--- mhcdemo/src/main/java/com/mhc/algorithm/Temp.java
+++ mhcdemo/src/main/java/com/mhc/algorithm/Temp.java
@@ -8,36 +8,28 @@ import java.util.*;
  */
 public class Temp {
 
-    public int maxSubArray(int[] nums) {
-        //define
-        int[] dp = new int[nums.length];
-
-        //init
-        dp[0] = nums[0];
-        int result = dp[0];
-
-        //function
-        for (int i = 1; i < nums.length; i++) {
-           dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            while (left < right && nums[right] > 1) {
+                right--;
+            }
+            while (left < right && nums[left] < 1) {
+                left++;
+            }
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            if (nums[left] == nums[right]) {
+                right--;
+            }
         }
-
-        //result
-        return result;
-    }
-
-
-    private int sum(int[] nums, int start, int end) {
-        int sum = 0;
-        for (int i = start; i <= end; i++) {
-            sum = sum + nums[i];
-        }
-        return sum;
     }
 
     public static void main(String[] args) {
         Temp temp = new Temp();
-        int i = temp.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
-        System.out.println(i);
+        temp.sortColors(new int[]{2,0,2,2,1,2,2,1,2,0,0,0,1});
     }
 
 }
